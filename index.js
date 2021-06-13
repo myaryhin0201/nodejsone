a48023d1b5c8cad6bf52d16808ee6d0d5c924df4
myaryhin0201 <yarygin45@gmail.com>
2021-06-13 13:54:15 +0200

    async invokeAction

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ const {
   getContactById,
   removeContact,
   addContact,
-} = require("./contacts");
-const { Command } = require("commander");
+} = require('./contacts');
+const { Command } = require('commander');
 
 const program = new Command();
 
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
-function invokeAction({ action, id, name, email, phone }) {
+async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
-      listContacts();
+    case 'list':
+      await listContacts();
       break;
 
-    case "get":
-      getContactById(id);
+    case 'get':
+      await getContactById(id);
       break;
 
-    case "add":
-      addContact(name, email, phone);
+    case 'add':
+      await addContact(name, email, phone);
       break;
 
-    case "remove":
-      removeContact(id);
+    case 'remove':
+      await removeContact(id);
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
 }
 
